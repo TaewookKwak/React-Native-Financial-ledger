9cbb68e1dcabe4b257bf8c6302af3f9d5ca7e997
Taewook <rhkrxodnr@gmail.com>
2022-10-31 23:59:53 +0900

    plus : Install Router

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Calendar from './pages/Calendar'
+import Main from './pages/Main'
 export default function App() {
+  const Stack = createNativeStackNavigator()
+
   return (
-    <View style={styles.container}>
-      <Text>첫번째 탭!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{ title: '하하하' }}
+        />
+        <Stack.Screen name="Calendar" component={Calendar} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
