d5c059c1ac02f678d6355638226f924906388cc1
Taewook <rhkrxodnr@gmail.com>
2022-12-13 18:04:24 +0900

    config : configure absolute path

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,35 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+  }
+}
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@pages': './src/pages',
+          '@assets': './src/assets',
+        },
+      },
+    ],
+  ],
+}
